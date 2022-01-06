11f2e831d68d3670d9576f1e138ff9af5438f432
Dariia Romanova <dariia.romanova@gmail.com>
2022-01-06 20:31:44 +0200

    Form fixes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function Form({ changeFieldSize, modes }) {
-  const [mode, setMode] = useState('');
+  const [size, setSize] = useState(0);
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    changeFieldSize(mode.fieldSize);
+    event.preventDefault(0);
+    changeFieldSize(size);
   };
 
   return (
     <form>
-      <select value={mode} onChange={(event) => setMode(event.target.value)}>
+      <select value={size} onChange={(event) => setSize(event.target.value)}>
+        <option value="0" disabled>Choose mode</option>
         {modes.map((chosenMode) => (
-          <option key={chosenMode.mode}>
+          <option key={chosenMode.mode} value={chosenMode.fieldSize}>
             {chosenMode.mode}
           </option>
         ))}
@@ -22,6 +23,7 @@ function Form({ changeFieldSize, modes }) {
       <button
         type="submit"
         onClick={handleSubmit}
+        disabled={!size}
       >
         Start
       </button>
